8c1c0d0ff730e487c6b8fa6b27ab1fb4545c088e
pskalskii <pawelskalski97@gmail.com>
2022-04-30 12:30:42 +0200

    suma zmiennych

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let firstNumber = 8;
+
+let secondNumber = 66;
+
+function addition(num1, num2) {
+  let result = num1 + num2;
+
+  return result;
+}
+
+console.log(addition(firstNumber, secondNumber));
